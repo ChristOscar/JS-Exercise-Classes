990094a7de5741bf9b2232a256361d5a9686be9e
Christwide Oscar <triple101.games@gmail.com>
2021-11-11 16:17:36 -0600

    Finished task 6: Project Manager

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ class Car {
     if(distance < driveMiles){
       this.odometer += distance;
       this.tank -= (this.odometer / this.milesPerGallon);
-    }else if(distance >= driveMiles) {
+    }else if(distance >= driveMiles) { // or else if(diveMiles === distance > miles)
       this.tank = 0;
       this.odometer += driveMiles;
       return `I ran out of fuel at ${this.odometer} miles!`;
@@ -202,8 +202,20 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+  constructor(obj){
+    super (obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp (slackChannel){
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+  }
+  debugsCode (student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
 /*
   STRETCH PROBLEM (no tests!)
